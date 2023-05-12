7f5e32c15e17d3d137eeaeda12b6af54b9fb8f69
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-05-12 17:07:06 -0400

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,28 @@
 import {defineCollection, z} from 'astro:content';
 
+const workCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		publishDate: z.coerce.date(),
+		tags: z.array(z.string()),
+		img: z.string(),
+		img_alt: z.string().optional(),
+	}),
+});
+
+const exhibitionCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		publishDate: z.coerce.date(),
+		tags: z.array(z.string()),
+		img: z.string(),
+		img_alt: z.string().optional(),
+	}),
+});
+
 export const collections = {
-	work: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
-	exhibition: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
+	work: workCollection,
+	exhibition: exhibitionCollection,
 };
