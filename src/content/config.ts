b96ae7c2112abccaaf54f3ea799d6a8a63cbc439
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-12-13 15:26:30 -0500

    projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,7 @@
-import {defineCollection, z} from 'astro:content';
+import {defineCollection, z} from "astro:content";
 
 export const collections = {
-	artwork: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
-	}),
-	exhibition: defineCollection({
+	project: defineCollection({
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
