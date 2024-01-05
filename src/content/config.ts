9eadd2b54e9ea73cd6d1010cc2c91dd0d5571c7f
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-05 11:12:15 -0500

    Mobile fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,16 @@
 import {defineCollection, z} from "astro:content";
 
-export const collections = {
-	project: defineCollection({
-		schema: z.object({
-			title: z.string(),
-			publishDate: z.coerce.date(),
-			subtitle: z.string().optional(),
-			description: z.string(),
-			url: z.string(),
-			images: z.array(z.string()).optional(),
-		}),
+const projectCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		publishDate: z.coerce.date(),
+		subtitle: z.string().optional(),
+		description: z.string(),
+		url: z.string(),
+		images: z.array(z.string()).optional(),
 	}),
+});
+
+export const collections = {
+	project: projectCollection,
 };
