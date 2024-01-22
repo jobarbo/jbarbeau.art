5aa144dcf8b2ca97a6d97fbfdc712a3409c284be
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-22 12:41:03 -0500

    updated and added tailwindcss

--- src/utils/contentCollections.ts
+++
@@ -1,7 +0,0 @@
-import { CollectionEntry, getCollection} from "astro:content";
-
-export const getSortedProjects = async (): Promise<CollectionEntry<"project">[]> => {
-  return (await getCollection("project")).sort((a, b) => 
-    new Date(b.data.publishDate).valueOf() - new Date(a.data.publishDate).valueOf()
-  );
-};
