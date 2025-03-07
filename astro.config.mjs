3ad50ef9f99937167dfa2dea497e06b3b1138501
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-03-06 22:13:43 -0500

    added open graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://jbarbeau.art",
 	integrations: [tailwind()],
 	devToolbar: {
 		enabled: false,
