5b8c78138242f90dbb9d0efa18f2d09b38403a04
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-03-28 00:42:08 -0400

    file and module cleanup

--- src/components/contentCollections.ts
+++
@@ -1,8 +0,0 @@
-import {getCollection} from "astro:content";
-import type {CollectionEntry} from "astro:content";
-
-export const test = "test";
-
-export const getSortedProjects = async (): Promise<CollectionEntry<"project">[]> => {
-	return (await getCollection("project")).sort((a, b) => new Date(b.data.publishDate).valueOf() - new Date(a.data.publishDate).valueOf());
-};
