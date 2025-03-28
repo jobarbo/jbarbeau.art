79c385608a7247022ab99f1b8aa7a3169ad46517
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-03-27 21:02:44 -0400

    removed redirects

--- public/admin/index.js
+++ public/admin/index.js
@@ -1,3 +1,3 @@
 // This file is just to ensure the admin directory is properly handled
 // Redirect to index.html if accessed directly
-window.location.replace("index.html");
+//window.location.replace("index.html");
