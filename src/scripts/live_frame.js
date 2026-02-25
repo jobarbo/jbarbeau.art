b8fe8d77305b52e15813455f2652a2e0aba15ee7
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2026-02-25 12:11:44 -0500

    fiixed frame height on responsive

--- src/scripts/live_frame.js
+++ src/scripts/live_frame.js
@@ -2,9 +2,9 @@
 export class LiveFrame {
 	constructor() {
 		this.iFrameArray = [
-			{url: "https://kodamandala.netlify.app/", urlParam: "fxhash", aspectRatio: "1/1", name: "Kodamandala - working title"},
+			{url: "https://kodamandala.netlify.app/", urlParam: "fxhash", aspectRatio: "1/1", name: "Kodamandala - 2026"},
 			{url: "https://jeudeligne.netlify.app/", urlParam: "hash", aspectRatio: "1/1.3756", name: "Jeu de ligne - 2023"},
-			{url: "https://ex-lignis.netlify.app/", urlParam: "fxhash", aspectRatio: "1/1.25", name: "EX-LIGNIS (working title)"},
+			{url: "https://ex-lignis.netlify.app/", urlParam: "fxhash", aspectRatio: "1/1.25", name: "EX-LIGNIS - 2026"},
 		];
 
 		this.currentUrl = "";
