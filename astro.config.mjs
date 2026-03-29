e35a1195eba762d85c5c10d482b3818dcf4dccf4
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2026-03-29 11:40:25 -0400

    image optimisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,6 +34,12 @@ export default defineConfig({
 			],
 		}),
 	],
+	image: {
+		remotePatterns: [
+			{protocol: "https", hostname: "substackcdn.com"},
+			{protocol: "https", hostname: "substack-post-media.s3.amazonaws.com"},
+		],
+	},
 	devToolbar: {
 		enabled: false,
 	},
